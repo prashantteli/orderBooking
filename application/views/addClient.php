4fb02df7bbcc2cc1e64629876b2743eef241169e
rac-rent-02 <prashantteli@ymamil.com>
2016-01-21 09:33:34 +0530

    file added

---
+++ application/views/addClient.php
@@ -0,0 +1,41 @@
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                Sales <small></small>
+            </h1>
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i>Sales
+                </li>
+                <li class="active">
+                    Add Client
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><i class="fa fa-clock-o fa-fw"></i> Tasks Panel</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="col-xs-12 col-sm-6 col-md-6">
+                        <div class="form-group">
+                            <input class="form-control" />
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
+            <!-- /.container-fluid -->
